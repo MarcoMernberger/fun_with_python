29443f317d4fb27d2ac29953a2a5bd54eef8bed6
Marco Mernberger <marco.mernberger@uni-marburg.de>
2024-08-12 13:07:06 +0200

    added jupyter notebooks

---
+++ data/lumberjack.py
@@ -0,0 +1,9 @@
+# a simple script
+print("I'm a lumberjack, and I'm okay")
+print("I sleep all night and I work all day")
+print("He's a lumberjack, and he's okay")
+print("He sleeps all night and he works all day")
+print("I cut down trees, I eat my lunch")
+print("I go to the lavatory")
+print("On Wednesdays I go shoppin'")
+print("And have buttered scones for tea")
